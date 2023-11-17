2f3533dded7b1032dabf6c35d0d0da4efca8a1ad
David Wolfson <145220120+WolfieKnee@users.noreply.github.com>
2023-11-17 14:08:09 +0000

    Update _config.js

--- _config.js
+++ _config.js
@@ -4,11 +4,11 @@ const site = lume();
 // Copy the "img" directory to _site/img
 site.copy("img");
 
-// use Open Innovations charting viz https://open-innovations.github.io/oi-lume-viz/
-import oiCharts from 'oi-lume-charts/mod.ts';
-site.use(oiCharts({
-  assetPath: 'assets/oi',
-  componentNamespace: 'oi',
-}));
+// // use Open Innovations charting viz https://open-innovations.github.io/oi-lume-viz/
+// import oiCharts from 'oi-lume-charts/mod.ts';
+// site.use(oiCharts({
+//   assetPath: 'assets/oi',
+//   componentNamespace: 'oi',
+// }));
 
 export default site;
