3ec6d0bace5c61da9915c7406cb86345819127a7
David Wolfson <145220120+WolfieKnee@users.noreply.github.com>
2023-11-17 13:59:27 +0000

    Update _config.js
    adding oOI Lume Viz https://open-innovations.github.io/oi-lume-viz/

--- _config.js
+++ _config.js
@@ -1,8 +1,14 @@
 import lume from "lume/mod.ts";
-
 const site = lume();
 
 // Copy the "img" directory to _site/img
 site.copy("img");
+
+// use Open Innovations charting viz https://open-innovations.github.io/oi-lume-viz/
+import oiCharts from 'oi-lume-charts/mod.ts';
+site.use(oiCharts({
+  assetPath: 'assets/oi',
+  componentNamespace: 'oi',
+}));
 
 export default site;
